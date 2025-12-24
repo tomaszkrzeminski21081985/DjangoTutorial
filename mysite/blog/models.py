f87dfb0bf8cfdb15b33bb7494dc2db7b41b32c47
tomaszkrzeminski21081985 <50801106+tomaszkrzeminski21081985@users.noreply.github.com>
2025-12-24 11:56:37 +0100

    added mirgration

---
+++ mysite/blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT='DF','Roboczy'
+        PUBLISHED='PB','Opublikowany'
+
+    title=models.CharField(max_length=250)
+    slug=models.SlugField(max_length=250)
+    body=models.TextField()
+    publish=models.DateTimeField(default=timezone.now)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    status=models.CharField(max_length=2,choices=Status,default=Status.DRAFT)
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             related_name='blog_posts')
+class Meta:
+    ordering=['-publish']
+    indexes=[models.Index(fields=['-publish'])]
+
+def __str__(self):
+    return self.title    
