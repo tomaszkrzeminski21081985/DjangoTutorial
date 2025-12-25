ac8e0016120d11eb927ac6091a55dd5f644dab58
tomaszkrzeminski21081985 <50801106+tomaszkrzeminski21081985@users.noreply.github.com>
2025-12-25 12:26:13 +0100

    created blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED) 
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT='DF','Roboczy'
@@ -17,6 +21,9 @@ class Post(models.Model):
     author=models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE,
                              related_name='blog_posts')
+    objects=models.Manager()
+    published=PublishedManager()
+
     class Meta:
         ordering=['-publish']
         indexes=[models.Index(fields=['-publish'])]
