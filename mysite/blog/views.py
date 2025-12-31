5be7ffdf71502bcb7c1be156b37cb9daf7d85c61
tomaszkrzeminski21081985 <50801106+tomaszkrzeminski21081985@users.noreply.github.com>
2025-12-31 09:18:20 +0100

    finish

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,10 +5,17 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm,SearchForm
 from django.core.mail import send_mail
 from django.views .decorators.http import require_POST
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import TrigramSimilarity
+from django.contrib.postgres.search import(
+    SearchVector,
+    SearchQuery,
+    SearchRank
+)
 
 def post_list(request,tag_slug=None):
     post_list=Post.published.all()
@@ -132,3 +139,32 @@ def post_comment(request,post_id):
             'comment':comment
         }
     )    
+
+
+def post_search(request):
+    form=SearchForm()
+    query=None
+    results=[]
+
+    if 'query' in request.GET:
+        form=SearchForm(request.GET)
+        if form.is_valid():
+            query=form.cleaned_data['query']
+            search_vector=SearchVector('title',weight='A') + SearchVector('body',weight='B')
+            search_query=SearchQuery(query,config='spanish')
+            results=(
+                Post.published.annotate(
+                    similarity=TrigramSimilarity('title',query)
+                ).filter(similarity__gt=0.3).order_by('-similarity')
+            )
+
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form':form,
+            'query':query,
+            'results':results
+        }
+        
+    )        
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'taggit',
     'blog.apps.BlogConfig',
 ]
@@ -80,10 +81,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER':config('DB_USER'),
+        'PASSWORD':config('DB_PASSWORD'),
+        'HOST':config('DB_HOST')
     }
 }
 
@@ -129,7 +136,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-from decouple import config
+
 
 EMAIL_BACKEND = 'mysite.mail_backend.UnsafeEmailBackend'
 # EMAIL_BACKEND='mysite.mail_backend.VerifiedEmailBackend'
